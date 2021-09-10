14cf2f1321e045ef1557545a59f625c4df568493
Abdimalik2010 <90246359+Abdimalik2010@users.noreply.github.com>
2021-09-10 23:36:06 +0200

    created new file

---
+++ handle.py
@@ -0,0 +1,6 @@
+print(" Welcome to Djibouti? ")
+Savings = int(input("State the amount you would like to save yearly "))
+Time = int(input("Enter number of years you want to invest, please "))
+Rate = int(input(" Type interest rate you wish "))
+totalRatio = Savings * (1+ (Rate / 100)) * Time
+print ("You have enterered that you are going to save {} dollar each year in {} years with {} procent interest rate, your total saving is going be ".format(Savings, Time, Rate) + str(totalRatio))
