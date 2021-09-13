2b1520d27f28f5e879bd2bc45fb82972b1a99c86
jonnaliesel <jonnaliesel@gmail.com>
2021-09-13 16:04:06 +0200

    Add interest on interest code

---
+++ joli.py
@@ -0,0 +1,6 @@
+investment = float(input('\nHow much do you want to invest? '))
+annualReturn = float(input('How many percent is the anual return? ' ))/100
+years = int(input('How many years will you save? '))
+
+finalReturn = investment * (1 + annualReturn) ** years
+print('Your savings after ' + str(years) + ' years will be ' + str(int(finalReturn)) + ' SEK')
