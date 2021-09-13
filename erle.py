adc9f44e45077767642ceb317dc0b413cf60c64d
unknown <erik.levander@chasacademy.se>
2021-09-13 13:52:29 +0200

    kod tillagt

--- erle.py
+++ erle.py
@@ -1,1 +1,9 @@
-# nu skriver jag något
+savings = int(input( "Ange sparande: "))
+year = int(input( "Hur många år vill du spara? "))
+interrest = float(input("Ange räntan i %: "))
+# Ändrar % till decimal
+interrest = interrest / 100
+total_savings = savings * ((1 + interrest) ** year)
+# Rundar av till närmaste heltal
+total_savings = round(total_savings)
+print("Du har på " + str(year) + "år tjänat " + str(total_savings - savings) + "kr")
