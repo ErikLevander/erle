741b512e165bddaa0e67eca4388534993bc17524
Hardi <90245857+Hardi5@users.noreply.github.com>
2021-09-14 11:42:54 +0200

    Update hardi.py

--- hardi.py
+++ hardi.py
@@ -1,1 +1,16 @@
+# Ränta på ränta ez
 
+# Kapital, tid och ränta
+kapital = float(input('Ange kapital '))
+tid = float(input('Ange hur länge: '))
+ranta = float(input('Ange ranta: '))
+
+# Calcualtion
+enkel_ranta = (kapital*tid*ranta)/100
+ranta_pa_ranta = kapital * ( (1+ranta/100)**tid - 1)
+
+# Displaying result
+print('Enkel ranta : %f' % (enkel_ranta))
+print('Ranta på ranta: %f' %(ranta_pa_ranta))
+
+#By hardi
